2a83d1ebc4eb883f1cb54505f69d98aa406c9e41
StacyAkinyi <stacy.akinyi@strathmore.edu>
2026-07-04 14:36:04 +0300

    Code Client and Server file

--- labs/SocketProgramming/MyServer.java
+++ labs/SocketProgramming/MyServer.java
@@ -1,5 +1,45 @@
 package labs.SocketProgramming;
+import java.net.*;
+import java.io.*;
 
 public class MyServer {
-    
+    public static void main(String[] args) throws Exception {
+        // Creating a port for communication [cite: 33]
+        ServerSocket ss = new ServerSocket(5555);
+        System.out.println("Server Initiated, Waiting for Client to Connect...");
+
+        // Binding Client and Server on port 5555 [cite: 34]
+        Socket s = ss.accept();
+        System.out.println("Client Connected");
+
+        // Reading input from KeyBoard [cite: 35]
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        // Output and Input streams [cite: 35, 37]
+        OutputStream ostream = s.getOutputStream();
+        PrintWriter pw = new PrintWriter(ostream, true);
+        InputStream istream = s.getInputStream();
+        BufferedReader receive = new BufferedReader(new InputStreamReader(istream));
+
+        String clientMessage = "", serverMessage = "";
+        while (true) {
+            // Read client message [cite: 39]
+            clientMessage = receive.readLine();
+            System.out.println("Client: " + clientMessage);
+            if (clientMessage.equals("bye")) break;
+
+            // Server writing message [cite: 40]
+            System.out.print("Server: ");
+            serverMessage = br.readLine();
+            pw.println(serverMessage);
+            if (serverMessage.equals("bye")) break;
+        }
+        
+        // Closing all connections [cite: 42]
+        s.close();
+        ss.close();
+        istream.close();
+        ostream.close();
+        System.out.println("Connection Terminated");
+    }
 }
